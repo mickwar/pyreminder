4135f29cd1209edb161d4e965e4ae4fc2e4b56f0
mickey <mickey.a.warner@gmail.com>
2019-10-23 15:05:16 -0600

    make executable, add time limit option

--- reminder.py
+++ reminder.py
@@ -1,25 +1,51 @@
+#!/usr/bin/env python3
+
 from sys import argv
-from time import sleep
+from time import sleep, time
 from tkinter import messagebox, Tk
 
-
 # Parse arguments
-if len(argv) > 1:
-    timer = int(argv[1])
-else:
-    timer = 10
+if len(argv) < 2:
+    print("")
+    print("Usage: reminder.py length [total] [message]")
+    print("")
+    print("Opens a pop-up menu displaying `message` every `length` minutes until `total` minutes have elapsed")
+    print("Leave `total` blank for one time use. Use `total` = inf for an indefinite period of time")
+    print("`message` defaults to 'Take a break'")
+    print("")
+    quit()
 
-if len(argv) > 2:
-    message = argv[2]
+length = float(argv[1])
+
+if len(argv) == 3:
+    total = float(argv[2])
+else:
+    total = length
+
+if len(argv) == 4:
+    message = argv[3]
 else:
     message = "Take a break"
 
 
 # Main function
 if __name__ == "__main__":
-    # Loop until process exits
-    while True:
-        sleep(timer * 60)
+
+    if length > total and total > 0:
+        print("Note: `length` is greater than `total`, reminder will only pop-up once.")
+
+    if total == float('inf'):
+        print("Note: Reminders will pop-up indefinitely")
+
+    START = time()
+
+    # Loop until until time goes beyond given length
+    while time() < START + total * 60:
+        # Using sleep in conjunction with the messagebox means that the time
+        # from closing the pop-up until the next one will be length * 60, NOT
+        # that the message will pop-up at exactly the length * 60 intervals.
+        # So if a message is left up for a while, the next pop-up will be offset.
+        sleep(length * 60)
         window = Tk()
         window.wm_withdraw()
         messagebox.showinfo("Reminder", message)
